431ac6a8aebccced73983c2052f15065e723e9be
jung-co <dal_a2@naver.com>
2021-01-07 06:58:53 +0900

    210106

--- BOJ/BOJ_2751.java
+++ BOJ/BOJ_2751.java
@@ -19,13 +19,6 @@ class BOJ_2751 {
         
         for(int val : num) {
         	sb.append(val).append('\n');
-        }
-        
-        
-
-        System.out.println("\u250C\u252C\u2510");
-        System.out.println("\u251C\u253C\u2524");
-        System.out.println("\u2514\u2534\u2518");
-        System.out.println(sb);
+        } System.out.println(sb);
     }
 }
