a1ed426d7078999912235a76dc819ef378c6e03b
jung-co <dal_a2@naver.com>
2021-01-09 00:26:46 +0900

    210108

--- SWEA/SWEA_1954.java
+++ SWEA/SWEA_1954.java
@@ -11,10 +11,36 @@ public class SWEA_1954 {
 		for(int test_case=1; test_case<=t; test_case++) {
 			StringBuilder sb = new StringBuilder();
 			sb.append("#").append(test_case).append("\n");
-			int n = Integer.parseInt(br.readLine()) - 1;
-			int[][] map = new int[n+1][n+1];
+			int n = Integer.parseInt(br.readLine());
+			int[][] map = new int[n][n];
+
+			int oper = 1;
+			int num = 1;
+			int x_idx = -1, y_idx = 0;
+			while(n > 0) {
+				for(int i=0; i<n; i++) {
+					x_idx+=oper;
+					map[y_idx][x_idx] = num;
+					num++;
+				}
+				
+				n--;
+				for(int i=0; i<n; i++) {
+					y_idx+=oper;
+					map[y_idx][x_idx] = num;
+					num++;
+				}
+				
+				oper *= -1;
+			}
 			
+			for(int i=0; i<map.length; i++) {
+				for(int j=0; j<map[0].length; j++) {
+					sb.append(map[i][j]).append(" ");
+				} sb.append("\n");
+			}
 			
+			System.out.print(sb);
 		}
 	}
 }
