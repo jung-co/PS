2e094c0a055a0e4e0c25ed295d7366b892b957e5
jung-co <dal_a2@naver.com>
2021-01-20 03:16:11 +0900

    210119

---
+++ BOJ/BOJ_10989.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_10989 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
